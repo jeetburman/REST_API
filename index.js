4b6f876ee6d1b2ab888950b7ebd7b950dc11335a
Jeet Burman <jeetburrman@gmail.com>
2025-11-11 22:48:40 +0530

    Day-2 updates

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const categoryRouter = require('./routes/category.route');
 
 const app = express();
 app.use(express.json());
+app.use('/categories',categoryRouter)
 const PORT = 7000;
 app.listen(PORT,()=>{console.log(`Server running on PORT : http://localhost:7000`)});
