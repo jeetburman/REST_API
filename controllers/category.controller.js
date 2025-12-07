05d7af291b74a0c032f61af06043c6199f67a817
Jeet Burman <jeetburrman@gmail.com>
2025-12-07 19:40:39 +0530

    Built GET, UPDATE, DELETE routes for category section

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -9,6 +9,9 @@ exports.createCategory = async (req,res)=>{
                 error : "name is needed"
             })
         }
+        if(await prisma.category.findUnique({ where : { name : req.body.name } })){
+            return res.status(409).json({error : `${req.body.name} category already exists`})
+        }
         const newCategory = await prisma.category.create({
             data : {
                 name : req.body.name
@@ -19,4 +22,61 @@ exports.createCategory = async (req,res)=>{
             e : e.message
         })
     }
+}
+
+exports.getCategories = async (req,res)=>{
+    try{
+        const categories = await prisma.category.findMany()
+
+        return res.status(200).json(categories)
+    }catch(e){
+        return res.status(500).json({e : e.message})
+    }
+}
+
+exports.updateCategory = async(req,res)=>{
+    try{
+        const categoryExists = await prisma.category.findUnique({ where: { id : parseInt(req.params.id) }});
+        if(!categoryExists){
+            return res.status(404).json({ e : 'Category not Found!'})
+        }
+        if(!req.body.name){
+            return res.status(422).json({ e : 'Name is required!'})
+        }
+        if(await prisma.category.findUnique({ where: {name: req.body.name}})){
+            return res.status(409).json({ e : `${req.body.name} category already exists!`})
+        }
+        const updatedCategory = await prisma.category.update({
+            data: {
+                name : req.body.name,
+            },
+            where:{
+                id: parseInt(req.params.id)
+            }
+        })
+        return res.status(200).json({ msg : "updated" + updatedCategory})
+    }catch(e){
+        return res.status(500).json({e : e.message})
+    }
+}
+
+exports.deleteCategory = async (req,res)=>{
+    try{
+        const categoryExists = await prisma.category.findUnique({ where: { id : parseInt(req.params.id) }});
+        if(!categoryExists){
+            return res.status(404).json({ e : 'Category not Found!'})
+        }
+        if(!req.body.name){
+            return res.status(422).json({ e : 'Name is required!'})
+        }
+        await prisma.category.delete({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        })
+        return res.status(204).send()
+    }catch(e){
+        return res.status(500).json({e : e.message})
+    }
+
 }
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const categoryRouter = require('./routes/category.route');
 
 const app = express();
 app.use(express.json());
+
 app.use('/categories',categoryRouter)
+
 const PORT = 7000;
 app.listen(PORT,()=>{console.log(`Server running on PORT : http://localhost:7000`)});
