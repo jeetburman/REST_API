1d696e6b2a3f023930beb9c79aa0e929f1b01347
Jeet Burman <jeetburrman@gmail.com>
2025-12-08 21:22:08 +0530

    Added Product routes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const categoryRouter = require('./routes/category.route');
+const productRouter = require('./routes/product.route');
 
 const app = express();
 app.use(express.json());
 
 app.use('/categories',categoryRouter)
+app.use('/product',productRouter)
 
 const PORT = 7000;
 app.listen(PORT,()=>{console.log(`Server running on PORT : http://localhost:7000`)});
